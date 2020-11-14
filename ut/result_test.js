f707939eac9c6e64fb0709dc954b52d8b0ec3534
Milena <milelucero_09@hotmail.com>
2020-11-14 11:39:58 -0300

    Added integration testing

--- ut/result_test.js
+++ ut/result_test.js
@@ -4,12 +4,12 @@ const expect = require('chai').expect;
 const {I} = inject();
 
 Scenario('Verify a successful call', async () => {
-	const res = await I.sendGetRequest('/');
+	const res = await I.sendGetRequest('https://example-voting-app-vote.herokuapp.com/');
 	expect(res.status).to.eql(200);
 });
 
 Scenario('I vote cats!', (I) => {
-  I.amOnPage('/');
+  I.amOnPage('https://example-voting-app-vote.herokuapp.com/');
   I.see('Cats vs Dogs!', 'h3');
   I.click('Cats');
   I.click({css: 'button .a'});
