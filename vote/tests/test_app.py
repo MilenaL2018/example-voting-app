e320325574129b61db19718dd865d52be0887e6d
Milena <milelucero_09@hotmail.com>
2020-11-16 19:37:33 -0300

    Added url

--- vote/tests/test_app.py
+++ vote/tests/test_app.py
@@ -5,20 +5,12 @@ import unittest
 class TextService(unittest.TestCase):
 
   ## Replace with heroku api
-  url = 'http://localhost:5000/'
+  url = 'https://example-voting-app-vote.herokuapp.com/'
 
   def test_get_status_200(self):
     app.testing = True  
     tester = app.test_client(self)
     response = tester.get(self.url)
-    self.assertEqual(response.status_code, 200)
-
-  def test_POSTGRES_status_200(self):
-    payload={'vote': 'a'}
-    headers = {
-      'Cookie': 'voter_id=39e29be59e1d5c6'
-    }
-    response = requests.post(self.url, headers=headers, data=payload)
     self.assertEqual(response.status_code, 200)
 
   def test_POSTGRES_status_400(self):
@@ -29,7 +21,7 @@ class TextService(unittest.TestCase):
 
   def test_get_voter_id_cookie(self):
     validCookie='39e29be59e1d5c6'
-    payload={'vote': 'a'}
+    payload={'vote': 'b'}
     headers = {
       'Cookie': 'voter_id=39e29be59e1d5c6'
     }
