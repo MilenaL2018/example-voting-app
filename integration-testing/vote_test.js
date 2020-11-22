7470e4a8dfcac160927e1cce324428518562b2ed
Milena <milelucero_09@hotmail.com>
2020-11-21 21:52:38 -0300

    Changed tests

--- integration-testing/vote_test.js
+++ integration-testing/vote_test.js
@@ -4,6 +4,4 @@ Scenario('I vote cats!', ({ I }) => {
     I.amOnPage('https://example-voting-app-vote.herokuapp.com/');
     I.seeElement('//*[@id="a"]');
     I.seeElement('//*[@id="b"]');
-    I.scrollPageToBottom();
-    I.seeElement('//*[@id="hostname"');
   });
